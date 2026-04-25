0aeabb6c02121d4853da463629668d111932df42
Maria <Maria@300gmail.com>
2026-04-24 18:38:59 -0700

    segunda version

--- main.java
+++ main.java
@@ -2,7 +2,11 @@ public class main {
     public static void main(String[] args) {
         int i = 0;
         while (i < 5) {
+            if (i <4) {
             System.err.println("hola ");
+            }else {
+                System.out.println("hasta pronto");
+            }
             i++;
         }
      }
